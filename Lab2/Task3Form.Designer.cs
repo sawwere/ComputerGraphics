ff9a99f0cee490c29211682b7ca3bc74d54f9955
sawwere <sawer_13@mail.ru>
2023-09-16 13:47:25 +0300

    Complete Lab2.3

--- Lab2/Task3Form.Designer.cs
+++ Lab2/Task3Form.Designer.cs
@@ -32,8 +32,18 @@ namespace Lab2
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -42,7 +52,7 @@ namespace Lab2
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Load";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -64,11 +74,79 @@ namespace Lab2
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(95, 415);
+            this.trackBar1.Maximum = 360;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 3;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(205, 415);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 4;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(315, 415);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 399);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "H";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "S";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(363, 399);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "V";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Draw";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Task3Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
@@ -76,7 +154,11 @@ namespace Lab2
             this.Text = "Task3Form";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +167,12 @@ namespace Lab2
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Lab2/Task3Form.cs
+++ Lab2/Task3Form.cs
@@ -7,19 +7,131 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FastBitmap;
 
 namespace Lab2
 {
     public partial class Task3Form : Form
     {
+        private Bitmap buffer;
         public Task3Form()
         {
             InitializeComponent();
         }
 
+        private (double, double, double) ConvertHSVtoRGB(double h, double s, double v)
+        {
+            s *= 100;
+            v *= 100;
+            int hi = ((int)Math.Floor(h / 60)) % 6;
+            double vmin = ((100 - s) * v / 100);
+            double a = (v - vmin) * ((int)h % 60) / 60;
+            double vinc = vmin + a;
+            double vdec = v - a;
+
+            v *= 2.55;
+            vinc *= 2.55;
+            vdec *= 2.55;
+            vmin *= 2.55;
+
+
+            switch (hi)
+            {
+                case 0:
+                    return (v, vinc, vmin);
+                case 1:
+                    return (vdec, v, vmin);
+                case 2:
+                    return (vmin, v, vinc);
+                case 3:
+                    return (vmin, vdec, v);
+                case 4:
+                    return (vinc, vmin, v);
+                case 5:
+                    return (v, vmin, vdec);
+                default:
+                    return (0, 0, 0);
+            }
+        }
+
+        private (double, double, double) ConvertRGBtoHSV(double r, double g, double b)
+        {
+            double MAX = (new double[] { r, g, b}).Max();
+            double MIN = (new double[] { r, g, b }).Min();
+            double h, s, v;
+            if (Math.Abs(MAX - MIN) < 0.0001)
+            {
+                h = 0;
+            }
+            else if (Math.Abs(MAX - r) < 0.0001 && g >= b)
+            {
+                h = 60 * ((g - b) / (MAX - MIN));
+            }
+            else if (Math.Abs(MAX - r) < 0.0001 && g < b)
+            {
+                h = 60 * ((g - b) / (MAX - MIN)) + 360;
+            }
+            else if (Math.Abs(MAX - g) < 0.0001)
+            {
+                h = 60 * ((b - r) / (MAX - MIN)) + 120;
+            }
+            else
+            {
+                h = 60 * ((r - g) / (MAX - MIN)) + 240;
+            }
+
+            if (Math.Abs(MAX - 0) < 0.0001)
+            {
+                s = 0;
+            }
+            else
+            {
+                s = 1 - (MIN / MAX);
+            }
+
+            v = MAX;
+            return  (h, s, v);
+        }
+
+        void Draw()
+        {
+            double deltaH = trackBar1.Value;
+            double deltaS = trackBar2.Value / 100.0;
+            double deltaV = trackBar3.Value / 100.0;
+            Bitmap newImage = (Bitmap)buffer.Clone();
+
+            using (var fastBitmap = new FastBitmap.FastBitmap(newImage))
+            {
+                for (var x = 0; x < fastBitmap.Width; x++)
+                    for (var y = 0; y < fastBitmap.Height; y++)
+                    {
+                        var color = fastBitmap[x, y];
+                        var change = ConvertRGBtoHSV(color.R / 255.0, color.G / 255.0, color.B / 255.0);
+
+                        change.Item1 += deltaH;
+                        change.Item1 = change.Item1 % 360;
+                        change.Item2 += deltaS;
+                        change.Item2 = Math.Min(change.Item2, 1);
+                        change.Item3 += deltaV;
+                        change.Item3 = Math.Min(change.Item3, 1);
+
+                        var rgb = ConvertHSVtoRGB(change.Item1, change.Item2, change.Item3);
+                        fastBitmap[x, y] = Color.FromArgb((int)(rgb.Item1), (int)(rgb.Item2), (int)(rgb.Item3));
+                    }
+                
+            }
+            pictureBox2.Image = newImage;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox1.Image = (this.Owner as Form1)._image;
+            buffer = new Bitmap(pictureBox1.Image);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Draw();
         }
     }
 }

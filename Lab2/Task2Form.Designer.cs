c4168b03684553a12fa656280fff5a35c769ab3d
Анастасия Шагалова <shagalova.stasya@gmail.com>
2023-09-20 23:37:45 +0300

    task2 without bar charts

--- Lab2/Task2Form.Designer.cs
+++ Lab2/Task2Form.Designer.cs
@@ -29,12 +29,100 @@ namespace Lab2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(2, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 408);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(408, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(391, 408);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "red";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(164, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "green";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(245, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "blue";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Task2Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Task2Form";
             this.Text = "Task2Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Lab2/Task2Form.cs
+++ Lab2/Task2Form.cs
@@ -12,9 +12,61 @@ namespace Lab2
 {
     public partial class Task2Form : Form
     {
+        private Bitmap buffer;
         public Task2Form()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = (this.Owner as Form1)._image;
+            buffer = new Bitmap(pictureBox1.Image);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap newImage = (Bitmap)buffer.Clone();
+            using (var fastBitmap = new FastBitmap.FastBitmap(newImage))
+            {
+                for (var x = 0; x < fastBitmap.Width; x++)
+                    for (var y = 0; y < fastBitmap.Height; y++)
+                    {
+                        fastBitmap[x, y] = Color.FromArgb(fastBitmap[x,y].R,0,0);
+                    }
+
+            }
+            pictureBox2.Image = newImage;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap newImage = (Bitmap)buffer.Clone();
+            using (var fastBitmap = new FastBitmap.FastBitmap(newImage))
+            {
+                for (var x = 0; x < fastBitmap.Width; x++)
+                    for (var y = 0; y < fastBitmap.Height; y++)
+                    {
+                        fastBitmap[x, y] = Color.FromArgb(0, fastBitmap[x, y].G, 0);
+                    }
+
+            }
+            pictureBox2.Image = newImage;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap newImage = (Bitmap)buffer.Clone();
+            using (var fastBitmap = new FastBitmap.FastBitmap(newImage))
+            {
+                for (var x = 0; x < fastBitmap.Width; x++)
+                    for (var y = 0; y < fastBitmap.Height; y++)
+                    {
+                        fastBitmap[x, y] = Color.FromArgb(0, 0, fastBitmap[x, y].B);
+                    }
+
+            }
+            pictureBox2.Image = newImage;
         }
     }
 }

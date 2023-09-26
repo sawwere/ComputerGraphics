0df4ed6ecf937738e4589d89d045daeccf6385bb
Анастасия Шагалова <shagalova.stasya@gmail.com>
2023-09-26 18:11:41 +0300

    add bar charts

--- Lab2/Task2Form.Designer.cs
+++ Lab2/Task2Form.Designer.cs
@@ -29,14 +29,22 @@ namespace Lab2
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -52,7 +60,7 @@ namespace Lab2
             // 
             this.pictureBox2.Location = new System.Drawing.Point(408, 1);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(391, 408);
+            this.pictureBox2.Size = new System.Drawing.Size(300, 199);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
@@ -97,11 +105,48 @@ namespace Lab2
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(408, 206);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series4.ChartArea = "ChartArea1";
+            series4.Color = System.Drawing.Color.Red;
+            series4.Legend = "Legend1";
+            series4.Name = "Red";
+            series5.ChartArea = "ChartArea1";
+            series5.Color = System.Drawing.Color.Green;
+            series5.Legend = "Legend1";
+            series5.Name = "Green";
+            series6.ChartArea = "ChartArea1";
+            series6.Color = System.Drawing.Color.Blue;
+            series6.Legend = "Legend1";
+            series6.Name = "Blue";
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(365, 203);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(335, 415);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(271, 20);
+            this.textBox1.TabIndex = 9;
+            // 
             // Task2Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -112,7 +157,9 @@ namespace Lab2
             this.Text = "Task2Form";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -124,5 +171,7 @@ namespace Lab2
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Lab2/Task2Form.cs
+++ Lab2/Task2Form.cs
@@ -21,52 +21,66 @@ namespace Lab2
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox1.Image = (this.Owner as Form1)._image;
-            buffer = new Bitmap(pictureBox1.Image);
+            buffer = new Bitmap(pictureBox1.Image);           
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Bitmap newImage = (Bitmap)buffer.Clone();
+            int[] R = new int[256];
             using (var fastBitmap = new FastBitmap.FastBitmap(newImage))
             {
                 for (var x = 0; x < fastBitmap.Width; x++)
                     for (var y = 0; y < fastBitmap.Height; y++)
                     {
                         fastBitmap[x, y] = Color.FromArgb(fastBitmap[x,y].R,0,0);
+                        ++R[fastBitmap[x, y].R];
                     }
 
             }
             pictureBox2.Image = newImage;
+            chart1.Series["Red"].Points.DataBindY(R);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             Bitmap newImage = (Bitmap)buffer.Clone();
+            int[] G = new int[256];
             using (var fastBitmap = new FastBitmap.FastBitmap(newImage))
             {
                 for (var x = 0; x < fastBitmap.Width; x++)
                     for (var y = 0; y < fastBitmap.Height; y++)
                     {
                         fastBitmap[x, y] = Color.FromArgb(0, fastBitmap[x, y].G, 0);
+                        ++G[fastBitmap[x, y].G];
                     }
 
             }
             pictureBox2.Image = newImage;
+            chart1.Series["Green"].Points.DataBindY(G);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             Bitmap newImage = (Bitmap)buffer.Clone();
+            int[] B = new int[256];
             using (var fastBitmap = new FastBitmap.FastBitmap(newImage))
             {
                 for (var x = 0; x < fastBitmap.Width; x++)
                     for (var y = 0; y < fastBitmap.Height; y++)
                     {
                         fastBitmap[x, y] = Color.FromArgb(0, 0, fastBitmap[x, y].B);
+                        ++B[fastBitmap[x, y].B];
                     }
 
             }
             pictureBox2.Image = newImage;
+            chart1.Series["Blue"].Points.DataBindY(B);
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+ 
         }
     }
 }

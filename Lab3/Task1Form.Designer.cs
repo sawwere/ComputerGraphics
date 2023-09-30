be80cd2f5273005260fc5ff1742db1cc823abcc7
OlegTerV <82037057+OlegTerV@users.noreply.github.com>
2023-10-01 02:28:11 +0300

    Заготовка для 1го задания - рисовалка

--- Lab3/Task1Form.Designer.cs
+++ Lab3/Task1Form.Designer.cs
@@ -29,12 +29,106 @@ namespace Lab3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Заливка цветом";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Заливка картинкой";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 92);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 34);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab3.Properties.Resources.grid;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(189, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(599, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 34);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Текущий цвет";
+            // 
+            // Task1Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Task1Form";
             this.Text = "Task1Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Lab3/Task1Form.cs
+++ Lab3/Task1Form.cs
@@ -8,13 +8,111 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Tools;
+using Tools.FastBitmap;
+
 namespace Lab3
 {
     public partial class Task1Form : Form
     {
+
+        private Graphics g;
+        private Bitmap drawArea;
+        private bool Zajata=false;//ЛКМ зажата 
+        private bool flag = false;// чтобы рисовать далее (поле заливки цветом)
+        private bool flag2 = false;//чтобы рисовать далее (поле заливки картинкой) 
+        Point currentPoint;
+        Point prevPoint;
+
+
         public Task1Form()
         {
             InitializeComponent();
+            g = this.CreateGraphics();
+            g.Clear(Color.White);
+            drawArea = new Bitmap(pictureBox1.Image);
+            g = Graphics.FromImage(drawArea);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)//заливка цветом
+        {
+            flag = !flag;
+            if (flag)
+                button2.Text = "Рисовать далее";
+            else
+                button2.Text = "Заливка цветом";
+        }
+
+        private void button1_Click(object sender, EventArgs e)//выбор цвета
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            (sender as Button).BackColor = colorDialog1.Color;
+        }
+
+        private void button4_Click(object sender, EventArgs e)//clear
+        {
+            drawArea = new Bitmap(Lab3.Properties.Resources.grid);
+            pictureBox1.Image = drawArea;
+
+            drawArea = new Bitmap(pictureBox1.Image);
+            g = Graphics.FromImage(drawArea);
+
+            flag = false;
+            flag2 = false;
+            button2.Text = "Заливка цветом";
+            button3.Text = "Заливка картинкой";
+
+        }
+
+        /*начало блока дря рисования*/
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+                Zajata = true;
+                currentPoint = e.Location;
+            
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Zajata = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!flag)
+            {
+                if (Zajata)
+                {
+                    prevPoint = currentPoint;
+                    currentPoint = e.Location;
+                    paint();
+                }
+            }
+        }
+
+        private void paint()
+        {
+            Pen p = new Pen(Color.Black, 1);
+            g.DrawLine(p, prevPoint, currentPoint);
+            pictureBox1.Image = drawArea;
+        }
+        /*конец блока дря рисования*/
+
+
+        private void button3_Click(object sender, EventArgs e)//заливка картинкой
+        {
+            flag2 = !flag2;
+            if (flag2)
+                button3.Text = "Рисовать далее";
+            else
+                button3.Text = "Заливка картинкой";
         }
     }
 }

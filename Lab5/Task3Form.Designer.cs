541a010aaec64a8fcc2231d946e0d915fe3106c8
OlegTerV <82037057+OlegTerV@users.noreply.github.com>
2023-10-16 03:08:05 +0300

    Lab5_Task3

--- Lab5/Task3Form.Designer.cs
+++ Lab5/Task3Form.Designer.cs
@@ -29,12 +29,51 @@ namespace Lab5
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1017, 491);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(1017, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Task3Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1041, 561);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Task3Form";
             this.Text = "Task3Form";
+            this.Load += new System.EventHandler(this.Task3Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Lab5/Task3Form.cs
+++ Lab5/Task3Form.cs
@@ -12,9 +12,166 @@ namespace Lab5
 {
     public partial class Task3Form : Form
     {
+        private Graphics g;
+        private Bitmap drawArea;
+        public List<PointF> points = new List<PointF>();
+        public List<PointF> points_help = new List<PointF>();
+
+        int count =-1;
+        int count_help = -1;
+        public PointF[] result = new PointF[101];
+        public PointF[] result2 = new PointF[101];
         public Task3Form()
         {
             InitializeComponent();
+            g = this.CreateGraphics();
+            g.Clear(Color.White);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            drawArea = new Bitmap(pictureBox1.Image);
+            g = Graphics.FromImage(drawArea);
+
         }
+
+        private void Task3Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            drawArea = new Bitmap(pictureBox1.Image);
+            g = Graphics.FromImage(drawArea);
+            points.Clear();
+            points_help.Clear();
+            count = -1;
+            count_help = -1;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            g.FillEllipse(new SolidBrush(Color.Black), e.X-5, e.Y-5, 10, 10);           
+            pictureBox1.Image = drawArea;
+            points.Add(e.Location);
+            points_help.Add(e.Location);
+
+
+            count_help++;
+            count++;
+            if (count == 3)
+            {
+                first();
+            }
+            else if (count > 3)
+            {
+                Bezier();
+            }
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+        
+        void first()
+        {
+            int j = 0;
+            float step = 0.01f;
+            for (float t = 0; t <= 1; t += step)
+            {
+                float bx = points[0].X*(1-t)*(1-t)*(1-t) + 3* points[1].X * (1 - t) * (1 - t)*t+3* points[2].X * (1 - t)*t*t+ points[3].X *t*t*t;
+                float by = points[0].Y * (1 - t) * (1 - t) * (1 - t) + 3 * points[1].Y * (1 - t) * (1 - t) * t + 3 * points[2].Y * (1 - t) * t * t + points[3].Y * t * t * t;
+                result2[j] = new PointF(bx, by);
+                j++;
+            }
+            g.DrawLines(new Pen(Color.Blue), result2);
+            pictureBox1.Image = drawArea;
+        }
+
+        
+        void Bezier()
+        {
+            int j = 0;
+            float step = 0.01f;
+            g.DrawLines(new Pen(Color.White), result2);// плохо
+            g.DrawLines(new Pen(Color.White), result);//плохо
+            
+            /*
+            for (int i=0; i<101; i++)
+            {
+                if (((drawArea.GetPixel((int)result[i].X, (int)result[i].Y)).ToArgb() != Color.Black.ToArgb()) ||
+                    ((drawArea.GetPixel((int)result[i].X, (int)result[i].Y)).ToArgb() != Color.Blue.ToArgb()))
+                {
+                    drawArea.SetPixel((int)result[i].X, (int)result[i].Y, Color.White);
+                }
+                if (((drawArea.GetPixel((int)result2[i].X, (int)result2[i].Y)).ToArgb() != Color.Black.ToArgb()) ||
+                    ((drawArea.GetPixel((int)result2[i].X, (int)result2[i].Y)).ToArgb() != Color.Blue.ToArgb()))
+                {
+                    drawArea.SetPixel((int)result2[i].X, (int)result2[i].Y, Color.White);
+                }
+
+            }*/
+
+
+            result = result2;
+            PointF a, b, c, d, e;
+
+            b = points[count - 2];//бесполезно по сути
+            d = points[count - 1];//бесполезно по сути
+
+            float x =(points_help[count_help - 1].X+ points_help[count_help - 2].X)/2;//центр между n-2 и n-1
+            float y = (points_help[count_help - 1].Y + points_help[count_help - 2].Y) / 2;//центр между n-2 и n-1
+            b.X = x; b.Y = y;
+            float x2 = (points_help[count_help ].X + points_help[count_help-1].X) / 2;//центр между n-1 и n
+            float y2 = (points_help[count_help ].Y + points_help[count_help-1].Y) / 2;//центр между n-1 и n
+            d.X = x2; d.Y = y2;
+
+
+            a = points[count - 2];
+            c = points[count - 1];
+            e = points[count];
+
+
+            points.RemoveAt(count);
+            points.RemoveAt(count-1);
+            points.RemoveAt(count-2);
+
+            points.Add(a);
+            points.Add(b);
+            points.Add(c);
+            points.Add(d);
+            points.Add(e);
+            count = count + 2;
+
+            for (float t = 0; t <= 1; t += step)
+            {
+                float bx = points[count-6].X * (1 - t) * (1 - t) * (1 - t) + 3 * points[count-5].X * (1 - t) * (1 - t) * t + 3 * points[count-4].X * (1 - t) * t * t + points[count-3].X * t * t * t;
+                float by = points[count-6].Y * (1 - t) * (1 - t) * (1 - t) + 3 * points[count - 5].Y * (1 - t) * (1 - t) * t + 3 * points[count - 4].Y * (1 - t) * t * t + points[count - 3].Y * t * t * t;
+                result[j] = new PointF(bx, by);
+                j++;
+            }
+            g.DrawLines(new Pen(Color.Blue), result);
+            j = 0;
+            for (float t = 0; t <= 1; t += step)
+            {
+                float bx = points[count - 3].X * (1 - t) * (1 - t) * (1 - t) + 3 * points[count - 2].X * (1 - t) * (1 - t) * t + 3 * points[count - 1].X * (1 - t) * t * t + points[count].X * t * t * t;
+                float by = points[count-3].Y * (1 - t) * (1 - t) * (1 - t) + 3 * points[count-2].Y * (1 - t) * (1 - t) * t + 3 * points[count-1].Y * (1 - t) * t * t + points[count].Y * t * t * t;
+                result2[j] = new PointF(bx, by);
+                j++;
+            }
+            
+            g.DrawLines(new Pen(Color.Blue), result2);
+            pictureBox1.Image = drawArea;
+        }
+
     }
 }

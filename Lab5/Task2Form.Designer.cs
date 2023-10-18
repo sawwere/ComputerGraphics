9e5979dacbb131e1adf1e64fa362813b8496ff12
OlegTerV <shagalova.stasya@gmail.com>
2023-10-18 11:58:44 +0300

    Lab5_task2

--- Lab5/Task2Form.Designer.cs
+++ Lab5/Task2Form.Designer.cs
@@ -29,12 +29,199 @@ namespace Lab5
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(641, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(733, 76);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 23);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            238,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(733, 105);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(55, 23);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            238,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(733, 134);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(55, 23);
+            this.numericUpDown3.TabIndex = 3;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 1;
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown4.Location = new System.Drawing.Point(733, 163);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(55, 23);
+            this.numericUpDown4.TabIndex = 4;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(659, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 47);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(659, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 47);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(685, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Left H:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(677, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Right H:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(668, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Iterations:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(659, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Roughness:";
+            // 
+            // Task2Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Task2Form";
             this.Text = "Task2Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Lab5/Task2Form.cs
+++ Lab5/Task2Form.cs
@@ -12,9 +12,83 @@ namespace Lab5
 {
     public partial class Task2Form : Form
     {
+        Graphics g;
+        Pen p;
+        bool f;
+        private Color brown = Color.Red;
         public Task2Form()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            numericUpDown1.Maximum = pictureBox1.Height;
+            numericUpDown2.Maximum = pictureBox1.Height;
+            pictureBox1.BackColor = Color.White;
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            button2_Click(sender, e);
+            int c = (int)numericUpDown3.Value;
+            p = new Pen(Color.Blue, 2);
+            g.DrawLine(p, 0, (int)numericUpDown1.Value, pictureBox1.Width, (int)numericUpDown2.Value);
+            pictureBox1.Refresh();
+            midpoint_displ((int)numericUpDown1.Value, 0, (int)numericUpDown2.Value, pictureBox1.Width, c);
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private int Interpolate(int x0, int y0, int x1, int y1, int i)
+        {
+            if (x0 == x1)
+                return y0;
+            return y0 + ((y1 - y0) * (i - x0)) / (x1 - x0);
+        }
+
+        private void midpoint_displ(int h1, int x1, int h2, int x2, int c)
+        {
+            if (c > 0)
+            {
+                --c;
+                int dis_x = x2 - x1;
+                int dis_h = h2 - h1;
+                double l = Math.Sqrt(dis_x * dis_x + dis_h * dis_h);
+                Random r = new Random();
+                double h = (h1 + h2) / 2.0;
+                double dis_hh = h1 - h;
+                double l2 = l / 2.0;
+                int x = x1 + (int)Math.Round(Math.Sqrt(l2 * l2 - dis_hh * dis_hh));
+                int rand = r.Next((int)((-1) * numericUpDown4.Value * (int)Math.Round(l)), (int)(numericUpDown4.Value * (int)Math.Round(l)));
+                h += rand;
+
+                if (h < 0)
+                    h = 10;
+                if (h > pictureBox1.Height)
+                    h = pictureBox1.Height - 10;
+
+                var colorR = Interpolate(0, brown.R, (int)numericUpDown3.Value, 0, c);
+                var colorG = Interpolate(0, brown.G, (int)numericUpDown3.Value, 255, c);
+                var colorB = Interpolate(0, brown.B, (int)numericUpDown3.Value, 0, c);
+                var curPen = new Pen(Color.FromArgb(255, colorR, colorG, colorB));
+
+                g.DrawLine(curPen, x1, h1, x, (int)Math.Ceiling(h));
+                g.DrawLine(curPen, x, (int)Math.Ceiling(h), x2, h2);
+                pictureBox1.Refresh();
+                midpoint_displ(h1, x1, (int)Math.Ceiling(h), x, c);
+                midpoint_displ((int)Math.Ceiling(h), x, h2, x2, c);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Dispose();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+        }
+
     }
 }

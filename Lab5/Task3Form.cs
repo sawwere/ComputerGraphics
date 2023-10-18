85c25c9efca57fa7b243863ca030c2a7102f5208
OlegTerV <www.oleg2002ter@mail.ru>
2023-10-18 16:43:17 +0300

    Lab5_task3_fix

--- Lab5/Task3Form.cs
+++ Lab5/Task3Form.cs
@@ -17,7 +17,7 @@ namespace Lab5
         public List<PointF> points = new List<PointF>();
         public List<PointF> points_help = new List<PointF>();
 
-        int count =-1;
+        int count = -1;
         int count_help = -1;
         public PointF[] result = new PointF[101];
         public PointF[] result2 = new PointF[101];
@@ -53,7 +53,7 @@ namespace Lab5
 
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
-            g.FillEllipse(new SolidBrush(Color.Black), e.X-5, e.Y-5, 10, 10);           
+            g.FillEllipse(new SolidBrush(Color.Black), e.X - 5, e.Y - 5, 10, 10);
             pictureBox1.Image = drawArea;
             points.Add(e.Location);
             points_help.Add(e.Location);
@@ -81,14 +81,14 @@ namespace Lab5
         {
 
         }
-        
+
         void first()
         {
             int j = 0;
             float step = 0.01f;
             for (float t = 0; t <= 1; t += step)
             {
-                float bx = points[0].X*(1-t)*(1-t)*(1-t) + 3* points[1].X * (1 - t) * (1 - t)*t+3* points[2].X * (1 - t)*t*t+ points[3].X *t*t*t;
+                float bx = points[0].X * (1 - t) * (1 - t) * (1 - t) + 3 * points[1].X * (1 - t) * (1 - t) * t + 3 * points[2].X * (1 - t) * t * t + points[3].X * t * t * t;
                 float by = points[0].Y * (1 - t) * (1 - t) * (1 - t) + 3 * points[1].Y * (1 - t) * (1 - t) * t + 3 * points[2].Y * (1 - t) * t * t + points[3].Y * t * t * t;
                 result2[j] = new PointF(bx, by);
                 j++;
@@ -97,19 +97,15 @@ namespace Lab5
             pictureBox1.Image = drawArea;
         }
 
-        
+
         void Bezier()
         {
 
-            g.Clear(Color.White);
-            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            drawArea = new Bitmap(pictureBox1.Image);
-            g = Graphics.FromImage(drawArea);
+
             int j = 0;
             float step = 0.01f;
             g.DrawLines(new Pen(Color.White), result2);// плохо
-            g.DrawLines(new Pen(Color.White), result);//плохо
-            
+            g.DrawLines(new Pen(Color.White), result);//плохо          
 
 
             result = result2;
@@ -118,11 +114,11 @@ namespace Lab5
             b = points[count - 2];//бесполезно по сути
             d = points[count - 1];//бесполезно по сути
 
-            float x =(points_help[count_help - 1].X+ points_help[count_help - 2].X)/2;//центр между n-2 и n-1
+            float x = (points_help[count_help - 1].X + points_help[count_help - 2].X) / 2;//центр между n-2 и n-1
             float y = (points_help[count_help - 1].Y + points_help[count_help - 2].Y) / 2;//центр между n-2 и n-1
             b.X = x; b.Y = y;
-            float x2 = (points_help[count_help ].X + points_help[count_help-1].X) / 2;//центр между n-1 и n
-            float y2 = (points_help[count_help ].Y + points_help[count_help-1].Y) / 2;//центр между n-1 и n
+            float x2 = (points_help[count_help].X + points_help[count_help - 1].X) / 2;//центр между n-1 и n
+            float y2 = (points_help[count_help].Y + points_help[count_help - 1].Y) / 2;//центр между n-1 и n
             d.X = x2; d.Y = y2;
 
 
@@ -132,8 +128,8 @@ namespace Lab5
 
 
             points.RemoveAt(count);
-            points.RemoveAt(count-1);
-            points.RemoveAt(count-2);
+            points.RemoveAt(count - 1);
+            points.RemoveAt(count - 2);
 
             points.Add(a);
             points.Add(b);
@@ -144,8 +140,8 @@ namespace Lab5
 
             for (float t = 0; t <= 1; t += step)
             {
-                float bx = points[count-6].X * (1 - t) * (1 - t) * (1 - t) + 3 * points[count-5].X * (1 - t) * (1 - t) * t + 3 * points[count-4].X * (1 - t) * t * t + points[count-3].X * t * t * t;
-                float by = points[count-6].Y * (1 - t) * (1 - t) * (1 - t) + 3 * points[count - 5].Y * (1 - t) * (1 - t) * t + 3 * points[count - 4].Y * (1 - t) * t * t + points[count - 3].Y * t * t * t;
+                float bx = points[count - 6].X * (1 - t) * (1 - t) * (1 - t) + 3 * points[count - 5].X * (1 - t) * (1 - t) * t + 3 * points[count - 4].X * (1 - t) * t * t + points[count - 3].X * t * t * t;
+                float by = points[count - 6].Y * (1 - t) * (1 - t) * (1 - t) + 3 * points[count - 5].Y * (1 - t) * (1 - t) * t + 3 * points[count - 4].Y * (1 - t) * t * t + points[count - 3].Y * t * t * t;
                 result[j] = new PointF(bx, by);
                 j++;
             }
@@ -154,12 +150,17 @@ namespace Lab5
             for (float t = 0; t <= 1; t += step)
             {
                 float bx = points[count - 3].X * (1 - t) * (1 - t) * (1 - t) + 3 * points[count - 2].X * (1 - t) * (1 - t) * t + 3 * points[count - 1].X * (1 - t) * t * t + points[count].X * t * t * t;
-                float by = points[count-3].Y * (1 - t) * (1 - t) * (1 - t) + 3 * points[count-2].Y * (1 - t) * (1 - t) * t + 3 * points[count-1].Y * (1 - t) * t * t + points[count].Y * t * t * t;
+                float by = points[count - 3].Y * (1 - t) * (1 - t) * (1 - t) + 3 * points[count - 2].Y * (1 - t) * (1 - t) * t + 3 * points[count - 1].Y * (1 - t) * t * t + points[count].Y * t * t * t;
                 result2[j] = new PointF(bx, by);
                 j++;
             }
-            
+
             g.DrawLines(new Pen(Color.Blue), result2);
+            for (int i = 0; i <= count_help; i++)
+            {
+                g.FillEllipse(new SolidBrush(Color.Black), points_help[i].X - 5, points_help[i].Y - 5, 10, 10);
+                //pictureBox1.Image = drawArea;
+            }
             pictureBox1.Image = drawArea;
         }
 

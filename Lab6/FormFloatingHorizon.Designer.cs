3c1623738cb3de6a97fbdb663716f7742d0d369a
Texorami <48715952+Texorami@users.noreply.github.com>
2023-12-01 15:00:54 +0300

    add floating horizon

--- Lab6/FormFloatingHorizon.Designer.cs
+++ Lab6/FormFloatingHorizon.Designer.cs
@@ -31,10 +31,9 @@ namespace Lab6
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormFloatingHorizon));
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBoxAxis = new System.Windows.Forms.ComboBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.buttonClear = new System.Windows.Forms.Button();
             this.buttonBuild = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,50 +41,23 @@ namespace Lab6
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(736, 20);
+            this.label1.Location = new System.Drawing.Point(981, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.Size = new System.Drawing.Size(99, 25);
             this.label1.TabIndex = 32;
             this.label1.Text = "Функция";
-            // 
-            // comboBoxAxis
-            // 
-            this.comboBoxAxis.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
-            this.comboBoxAxis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.comboBoxAxis.ForeColor = System.Drawing.Color.SeaShell;
-            this.comboBoxAxis.FormattingEnabled = true;
-            this.comboBoxAxis.Items.AddRange(new object[] {
-            "X",
-            "Y",
-            "Z"});
-            this.comboBoxAxis.Location = new System.Drawing.Point(824, 12);
-            this.comboBoxAxis.Name = "comboBoxAxis";
-            this.comboBoxAxis.Size = new System.Drawing.Size(174, 28);
-            this.comboBoxAxis.TabIndex = 30;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Gray;
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(700, 700);
+            this.pictureBox1.Size = new System.Drawing.Size(933, 862);
             this.pictureBox1.TabIndex = 25;
             this.pictureBox1.TabStop = false;
-            // 
-            // buttonClear
-            // 
-            this.buttonClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
-            this.buttonClear.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
-            this.buttonClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.buttonClear.ForeColor = System.Drawing.Color.SeaShell;
-            this.buttonClear.Location = new System.Drawing.Point(740, 636);
-            this.buttonClear.Name = "buttonClear";
-            this.buttonClear.Size = new System.Drawing.Size(229, 35);
-            this.buttonClear.TabIndex = 34;
-            this.buttonClear.Text = "Очистить";
-            this.buttonClear.UseVisualStyleBackColor = false;
             // 
             // buttonBuild
             // 
@@ -95,32 +67,51 @@ namespace Lab6
             this.buttonBuild.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonBuild.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.buttonBuild.ForeColor = System.Drawing.Color.SeaShell;
-            this.buttonBuild.Location = new System.Drawing.Point(740, 677);
+            this.buttonBuild.Location = new System.Drawing.Point(987, 833);
+            this.buttonBuild.Margin = new System.Windows.Forms.Padding(4);
             this.buttonBuild.Name = "buttonBuild";
-            this.buttonBuild.Size = new System.Drawing.Size(229, 35);
+            this.buttonBuild.Size = new System.Drawing.Size(305, 43);
             this.buttonBuild.TabIndex = 33;
             this.buttonBuild.Text = "Построить";
             this.buttonBuild.UseVisualStyleBackColor = false;
+            this.buttonBuild.Click += new System.EventHandler(this.buttonBuild_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "x + y",
+            "x^2 + y^2",
+            "Sin(x) * Cos(Y)",
+            "Sin(x)",
+            "Cos(Sqrt(x^2 + y^2))",
+            "Sin(Sqrt(x^2 + y^2))"});
+            this.comboBox1.Location = new System.Drawing.Point(1116, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 24);
+            this.comboBox1.TabIndex = 35;
             // 
             // FormFloatingHorizon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(1007, 730);
-            this.Controls.Add(this.buttonClear);
+            this.ClientSize = new System.Drawing.Size(1343, 898);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.buttonBuild);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBoxAxis);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.SeaShell;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormFloatingHorizon";
             this.Text = "Метод плавающего горизонта";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormFloatingHorizon_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FormFloatingHorizon_KeyPress);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -131,8 +122,7 @@ namespace Lab6
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBoxAxis;
-        private System.Windows.Forms.Button buttonClear;
         private System.Windows.Forms.Button buttonBuild;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Lab6/FormFloatingHorizon.cs
+++ Lab6/FormFloatingHorizon.cs
@@ -7,14 +7,290 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tools;
+using Tools.Primitives;
+
 
 namespace Lab6
 {
     public partial class FormFloatingHorizon : Form
     {
-        public FormFloatingHorizon()
-        {
+        Mesh figure = null;
+        Graphic gra = null;
+        Graphics g;
+        public double phi = 60, psi = 100;
+        public FormFloatingHorizon() {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.TranslateTransform(pictureBox1.ClientSize.Width / 2, pictureBox1.ClientSize.Height / 2);
+            g.ScaleTransform(1, -1);
+        }
+
+
+
+
+        private void buttonBuild_Click(object sender, EventArgs e)
+        {
+            gra = new Graphic(comboBox1.SelectedIndex);
+
+            figure = gra;
+
+            g.Clear(Color.White);
+            //Graph.isGraph = true;
+            gra.picture = pictureBox1;
+            gra.DrawGraphic();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+        }
+
+        private void FormFloatingHorizon_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                if (e.KeyChar ==' ')
+                    psi -= 10;
+                else if (e.KeyChar == 'd')
+                    psi += 10;
+                else if (e.KeyChar == 'w')
+                    phi -= 10;
+                else if (e.KeyChar == 's')
+                    phi += 10;
+            gra.DrawGraphic();
+
+        }
+
+        private void FormFloatingHorizon_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if (e.KeyCode == Keys.Space)
+            //    psi -= 10;
+            //else if (e.KeyCode == Keys.D)
+            //    psi += 10;
+            //else if (e.KeyCode == Keys.W)
+            //    phi -= 10;
+            //else if (e.KeyCode == Keys.S)
+            //    phi += 10;
+            //gra.DrawGraphic();
         }
     }
+    class Graphic : Mesh
+    {
+        public Func<double, double, double> F;
+        public int X0 { get; }
+        public int X1 { get; }
+        public int Y0 { get; }
+        public int Y1 { get; }
+        public int CountOfSplits { get; }
+
+        public PictureBox picture;
+
+        public double phi = 60, psi = 100;
+
+        public List<Triangle3D> Polygons;
+
+        readonly Func<double, double, double> func = (x, y) => Math.Cos(Math.Sqrt(x * x + y * y));
+
+        //границы горизонта
+        List<double> upFloatingHorizon, downFloatingHorizon;
+
+        public Graphic(int func)
+        {
+            switch (func)
+            {
+                case 0:
+                    F = (x, y) => x + y;
+                    break;
+                case 1:
+                    F = (x, y) => (float)Math.Cos(x * x + y * y);
+                    break;
+                case 2:
+                    F = (x, y) => (float)Math.Sin(x) + (float)Math.Cos(y);
+                    break;
+                case 3:
+                    F = (x, y) => (float)Math.Sin(x);
+                    break;
+                case 4:
+                    F = (x, y) => Math.Cos(Math.Sqrt(x * x + y * y));
+                    break;
+                case 5:
+                    F = (x, y) => Math.Sin(Math.Sqrt(x * x + y * y));
+                    break;
+                default:
+                    F = (x, y) => x + y;
+                    break;
+            }
+        }
+
+        public void DrawGraphic()
+        {
+            //создаем и заполняем границы горизонта
+            upFloatingHorizon = new List<double>(picture.Width);
+            downFloatingHorizon = new List<double>(picture.Width);
+            for (int i = 0; i < picture.Width; i++)
+            {
+                upFloatingHorizon.Add(0);
+                downFloatingHorizon.Add(1000);
+            }
+
+            Bitmap res = new Bitmap(picture.Width, picture.Height);
+
+            for (double x = -5; x <= 5.001; x += 0.2)
+            {
+                List<Point> currentCurve = new List<Point>();
+                for (double y = -5; y <= 5.001; y += 0.2)
+                {
+                    double z = F(x, y);
+
+                    //отображение координат на данной проекции
+                    double _phi = phi * Math.PI / 180, _psi = psi * Math.PI / 180;
+                    double fx = x * Math.Cos(_psi) - (-Math.Sin(_phi) * y + Math.Cos(_phi) * z) * Math.Sin(_psi);
+                    double fy = y * Math.Cos(_psi) + z * Math.Sin(_phi);
+                    double k = 50d;
+
+                    //отображение на pictureBox
+                    currentCurve.Add(new Point((int)Math.Round(picture.Width / 2 + fx * k), (int)Math.Round(picture.Height / 2 + fy * k)));
+                }
+                DrawCurve(res, currentCurve, upFloatingHorizon, downFloatingHorizon);
+            }
+            if (picture.Image != null) picture.Image.Dispose();
+            picture.Image = res;
+        }
+
+        public void DrawCurve(Bitmap res, List<Point> curve, List<double> upFloatingHorizon, List<double> downFloatingHorizon)
+        {
+            for (int i = 1; i < curve.Count; i++)
+            {
+                Point p1 = curve[i - 1];
+                Point p2 = curve[i];
+
+                int x1 = p1.X, x2 = p2.X, y1 = p1.Y, y2 = p2.Y;
+                bool needChange = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+
+                if (needChange)
+                {
+                    (x1, y1) = (y1,x1);
+                    (x2, y2) = (y2, x2);
+                }
+                if (x1 > x2)
+                {
+                    (x1, x2) = (x2, x1);
+                    (y1, y2) = (y2, y1);
+                }
+
+                double df = (y2 * 1.0 - y1) / (x2 * 1.0 - x1); //находим градиент
+                double y = y1 + df;
+
+                for (int x = x1 + 1; x < x2; x++)
+                {
+                    int xx1 = needChange ? (int)Math.Round(y) : x;
+                    int xx2 = needChange ? (int)Math.Round(y) : x;
+                    int yy1 = needChange ? x : (int)Math.Round(y);
+                    int yy2 = needChange ? x : (int)Math.Round(y + 1);
+
+                    if (xx1 < 0 || xx2 < 0 || yy1 < 0 || yy2 < 0) continue;
+                    if (xx1 >= res.Width || xx2 >= res.Width || yy1 >= res.Height || yy2 >= res.Height) continue;
+
+                    if ((yy1 >= upFloatingHorizon[xx1] && yy2 >= upFloatingHorizon[xx2]))
+                    {
+                        res.SetPixel(xx2, yy2, Color.Black);
+                        upFloatingHorizon[xx1] = yy1;
+                        upFloatingHorizon[xx2] = yy2;
+                    }
+                    else if (yy1 <= downFloatingHorizon[xx1] && yy2 <= downFloatingHorizon[xx2])
+                    {
+                        res.SetPixel(xx1, yy1, Color.Black);
+                        res.SetPixel(xx2, yy2, Color.Black);
+                        downFloatingHorizon[xx1] = yy1;
+                        downFloatingHorizon[xx2] = yy2;
+
+                    }
+                    y += df;
+                }
+            }
+        }   
+    }
+
+    public sealed class ReverseFloatComparer : IComparer<float>
+    {
+        public int Compare(float x, float y)
+        {
+            return y.CompareTo(x);
+        }
+    }
+    class Graph : Mesh
+    {
+        public Func<double, double, double> F;
+        public int X0 { get; }
+        public int X1 { get; }
+        public int Y0 { get; }
+        public int Y1 { get; }
+        public int CountOfSplits { get; }
+        public List<Triangle3D> Polygons;
+        public Graph(int x0, int x1, int y0, int y1, int count, int func)
+        {
+            X0 = x0;
+            X1 = x1;
+            Y0 = y0;
+            Y1 = y1;
+            CountOfSplits = count;
+            Polygons = new List<Triangle3D>();
+
+            float dx = (Math.Abs(x0) + Math.Abs(x1)) / (float)count;
+            float dy = (Math.Abs(y0) + Math.Abs(y1)) / (float)count;
+
+            List<Point3D> points0 = new List<Point3D>();
+            List<Point3D> points = new List<Point3D>();
+
+            switch (func)
+            {
+                case 0:
+                    F = (x, y) => x + y;
+                    break;
+                case 1:
+                    F = (x, y) => (float)Math.Cos(x * x + y * y);
+                    break;
+                case 2:
+                    F = (x, y) => (float)Math.Sin(x) * 10f + (float)Math.Cos(y) * 10f;
+                    break;
+                case 3:
+                    F = (x, y) => (float)Math.Sin(x) * 5f;
+                    break;
+                case 4:
+                    F = (x, y) => x + (y * y);
+                    break;
+                default:
+                    F = (x, y) => x + y;
+                    break;
+            }
+
+            for (float x = x0; x < x1; x += dx)
+            {
+                for (float y = y0; y < y1; y += dy)
+                {
+                    var z = F(x, y);
+                    points.Add(new Point3D(x, y, (float)z));
+                }
+
+                if (points0.Count != 0)
+                    for (int i = 1; i < points0.Count; ++i)
+                    {
+                        Polygons.Add(new Triangle3D(
+
+                            new Point3D(points0[i - 1].X, points0[i - 1].Y, points0[i - 1].Z),
+                            new Point3D(points[i - 1].X, points[i - 1].Y, points[i - 1].Z),
+                            new Point3D(points[i].X, points[i].Y, points[i].Z)
+                         ));
+                        Polygons.Add(new Triangle3D(
+                           new Point3D(points[i - 1].X, points[i - 1].Y, points[i - 1].Z),
+                           new Point3D(points[i].X, points[i].Y, points[i].Z),
+                           new Point3D(points0[i].X, points0[i].Y, points0[i].Z)));
+                    }
+                points0.Clear();
+                points0 = points;
+                points = new List<Point3D>();
+            }
+        }
+    }
+
 }

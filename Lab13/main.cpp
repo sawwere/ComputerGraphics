eedeb3049544cc71d6cbd31e2821a52461d08405
sawwere <sawer_13@mail.ru>
2023-12-01 11:19:55 +0300

    Add Lab13 project

---
+++ Lab13/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    int taskCode = 0;
+    while (true)
+    {
+        std::cout << "Enter task number: ";
+        std::cin >> taskCode;
+        switch (taskCode)
+        {
+        case 0:
+            return 0;
+        default:
+            continue;;
+        }
+    }
+    return 0;
+}
